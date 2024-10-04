c548656eb5b93dba2355347c2d70968010efbb4e
Kunal-AiBi <kunal@aibisolutions.com>
2024-10-04 13:08:19 +0530

    screen init

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ import pygame
 from constants import *
 
 def main():
+
+    pygame.init()
+    running = True
+    if(pygame.get_init()):
+        screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            screen.fill("black")
+            pygame.display.flip()
+
     print("Starting asteroids!")
     print("Screen width:",SCREEN_WIDTH)
     print("Screen height:",SCREEN_HEIGHT) 
