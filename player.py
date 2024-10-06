a68bc1c6b1df399e982234c829a2c02da4105eab
Kunal-AiBi <kunal@aibisolutions.com>
2024-10-07 01:45:31 +0530

    fixed rotation and movement

--- player.py
+++ player.py
@@ -16,7 +16,7 @@ class Player(CircleShape):
   def draw(self, screen):
      pygame.draw.polygon(screen,color="white",points=self.triangle(),width=2)
   def rotate(self,dt):
-     self.rotation=PLAYER_TURN_SPEED*dt
+     self.rotation+=PLAYER_TURN_SPEED*dt
 
   def update(self, dt):
     keys = pygame.key.get_pressed()
@@ -27,7 +27,7 @@ class Player(CircleShape):
       self.rotate(dt)
     if keys[pygame.K_w]:
       self.move(dt)
-    if keys[pygame.K_x]:
+    if keys[pygame.K_s]:
       self.move(-dt)
   def move(self,dt):
     forward=pygame.Vector2(0,1).rotate(self.rotation)*PLAYER_SPEED*dt
