289d26dd6110e510b888361319f7848116bbffdd
Kunal-AiBi <kunal@aibisolutions.com>
2024-10-07 23:10:14 +0530

    added shooting timer

--- player.py
+++ player.py
@@ -6,7 +6,7 @@ class Player(CircleShape):
   def __init__(self,x,y):
     super().__init__(x,y,PLAYER_RADIUS)
     self.rotation=0
- 
+    self.timer=0
   def triangle(self):
       forward = pygame.Vector2(0, 1).rotate(self.rotation)
       right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -20,6 +20,7 @@ class Player(CircleShape):
      self.rotation+=PLAYER_TURN_SPEED*dt
 
   def update(self, dt):
+    self.timer-=dt
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_a]:
@@ -31,7 +32,11 @@ class Player(CircleShape):
     if keys[pygame.K_s]:
       self.move(-dt)
     if keys[pygame.K_SPACE]:
-      self.shoot()
+      if self.timer>0:
+        pass
+      else:
+        self.shoot()
+        self.timer=PLAYER_SHOOT_COOLDOWN
   def move(self,dt):
     forward=pygame.Vector2(0,1).rotate(self.rotation)*PLAYER_SPEED*dt
     self.position+=forward
